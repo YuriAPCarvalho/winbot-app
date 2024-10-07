f0579d8ec988c776fc89ce977cae76b089846120
alanarg <alanarguelho2@gmail.com>
2024-10-07 00:44:49 -0400

    Cors

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-//simple express server to run frontend production build;
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const app = express();
 const env = require("dotenv").config();
+
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(env.REACT_APP_PORT);
-console.log("Server is running on port " + env.REACT_APP_PORT);
+
+app.listen(env.REACT_APP_PORT, () => {
+  console.log("Server is running on port " + env.REACT_APP_PORT);
+});
